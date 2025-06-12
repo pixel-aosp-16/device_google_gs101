7ae1356de7a2e775b337939914493d573b74a0ca
flawedworld <flawedworld@flawed.world>
2025-06-12 20:53:25 +0000

    Build fix for missing SELinux policy - potential unfucking of fuckup
    Change-Id: I6554651543fecbee9a330563a36c184ef01c1ff0

--- device.mk
+++ device.mk
@@ -14,6 +14,8 @@
 # limitations under the License.
 #
 
+include device/google/gs-common/gps/brcm/device.mk
+
 include device/google/gs-common/device.mk
 include device/google/gs-common/gs_watchdogd/watchdog.mk
 include device/google/gs-common/ramdump_and_coredump/ramdump_and_coredump.mk
