7b6da4073962cab67b46afa4d5b7ba383a6c4a0c
flawedworld <flawedworld@flawed.world>
2025-06-14 03:09:47 +0000

    pkvm/
    unused

--- device.mk
+++ device.mk
@@ -895,7 +895,7 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, packages/modules/Virtualization/apex/product_packages.mk)
 PRODUCT_BUILD_PVMFW_IMAGE := true
 ifeq ($(TARGET_PKVM_ENABLED),true)
-    PRODUCT_PACKAGES += pkvm_enabler
+
 else
 
 endif
